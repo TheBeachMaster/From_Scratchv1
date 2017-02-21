a8f756e19164b95fb9d922984d8435938aa39781
KASPAROV84 <wilkit84@gmail.com>
2017-02-21 10:41:18 +0300

    New Changes

--- From_Scratchv1.ino
+++ From_Scratchv1.ino
@@ -27,27 +27,31 @@ void WriteMaster(); // Writes UID to Master File in SD card
 void WriteOrdinary(); // Writes UID to Ordinary File in SD card
 boolean IsMaster =  false; // Returns true if Scanned Tag's UID is found in Master File
 boolean IsOrdinary = false; //Set to true if file is not from Master file
+void CheckOrdinary();
+void CheckMaster();
+void SD_Enable();
+void SD_Disable();
 
 File myFile;
-byte readCard[4]; // Array to store UID of a Single Tag temporarily
+byte readCard[7]; // Array to store UID of a Single Tag temporarily
+char SD_buffer[10];
+char card_buffer[10];
+char write_buffer[10];
+uint8_t _match=0; // Flag for checking if there was a hit
 
-unsigned long DataBucket; // Temporarily Stores Scanned UID into a single variable
-String boss;
-
+char DataBucket[] ={}; // Temporarily Stores Scanned UID into a single variable
 
 void setup() {
-
   CommsInit(); // Initializes Serial and SPI communication Protocals
+  RFIDInit();
   LCDInit();
-  CreateFile(); // Creates files in SD Card
-
+  CreateFile(); // Creates files in SD Card 
 }
 
 void loop() {
-  // nothing happens after setup finishes.
   ReadUID();
-  //CheckMatch();
-  
+  //CheckMaster();
+  //WriteMaster();
 }
 void CreateFile() {
   pinMode(SD_POWER, OUTPUT);
@@ -78,8 +82,8 @@ void CreateFile() {
 
 void RFIDInit() {//Creates an Instance of RFID Object called rfid and initializes it
   //Starting RFID and stopping SD
-  digitalWrite(SD_CS, HIGH);
-  digitalWrite(SD_POWER, HIGH);
+  //digitalWrite(SD_CS, HIGH);
+ // digitalWrite(SD_POWER, HIGH);
   pinMode(RFID_SS, OUTPUT);
   digitalWrite(RFID_SS, LOW);
   rfid.PCD_Init();
@@ -94,8 +98,9 @@ void CommsInit() {
   }
 }
 void ReadUID() {
-  //Stopping SD Card in readiness for RFID initialization
   RFIDInit();
+  //Stopping SD Card in readiness for RFID initialization
+  SD_Disable();
   // Getting ready for Reading Tags/Cards
   if ( ! rfid.PICC_IsNewCardPresent()) { //If a new Access Card is placed to RFID reader continue
     return 0;
@@ -108,26 +113,61 @@ void ReadUID() {
 
   for (int i = 0; i < 4; i++) {
     readCard[i] = rfid.uid.uidByte[i];
-    Serial.print(readCard[i], HEX);
+     Serial.print("readCard: ");
+    Serial.println(readCard[i], HEX);
+    
+    byte lowerByte = (readCard[i]&0x0F);
+    
+//    Serial.print("Lower: ");
+//    Serial.println(lowerByte,HEX);
 
+    int lowerInt = (int) lowerByte; //converting byte into int
+//    Serial.print("Lower int: ");
+//    Serial.println(lowerInt);
+
+    char lowerChar[1];
+    itoa(lowerInt,lowerChar,16);
+    Serial.print("Lower char: ");
+    Serial.println(lowerChar);
+
+    byte upperByte = (readCard[i] >> 4);
+    
+//    Serial.print("Upper: ");
+//    Serial.println(upperByte,HEX);
+
+    int upperInt = (int) upperByte;
+//    Serial.print("Upper int: ");
+//    Serial.println(upperInt);
+
+    char upperChar[1];
+    itoa(upperInt,upperChar,16);
+    Serial.print("upper char: ");
+    Serial.println(upperChar);
+    int j=i*2;
+    int k=j+1;
+    card_buffer[k] = lowerChar[0];
+    card_buffer[j] = upperChar[0];
+//    Serial.print("buffer: ");
+//    Serial.println(card_buffer[i]);
+    
+    
   }
-  
-      DataBucket = rfid.uid.uidByte[0] ;
-      DataBucket += rfid.uid.uidByte[1] ;
-      DataBucket += rfid.uid.uidByte[2] ;
-      DataBucket += rfid.uid.uidByte[3];
-      Serial.println("");
-      Serial.println("******************************");
- CheckMatch();
-      Serial.println("******************************");
+  Serial.println(card_buffer);
+    for(int i=0; i<8; i++){
+      
+    }
+      DataBucket[0] = rfid.uid.uidByte[0], HEX << 24;
+      DataBucket[1] = rfid.uid.uidByte[1], HEX << 16;
+      DataBucket[2] = rfid.uid.uidByte[2], HEX << 8;
+      DataBucket[3] = rfid.uid.uidByte[3], HEX;
+      //Serial.println(DataBucket[2]);
+     WriteMaster();
       rfid.PICC_HaltA(); // Stop reading
-  return 1;
+      return 1;
+      
 }
 void WriteMaster() {
-  pinMode(SD_CS, OUTPUT);
-  digitalWrite(SD_CS, LOW);
-  pinMode(SD_POWER, OUTPUT);
-  digitalWrite(SD_POWER, LOW);
+  SD_Enable();
   Serial.println("Initializing SD card...");
 
   if (!SD.begin(SD_CS)) {
@@ -140,17 +180,36 @@ void WriteMaster() {
   if (myFile) {
     Serial.print("Writing to Master.txt...");
 
-    //     myFile.print(readCard[0]);
-    //     myFile.print(readCard[1]);
-    //     myFile.print(readCard[2]);
-    //     myFile.print(readCard[3]);
+//    myFile.print(readCard[0], HEX);
+//    myFile.print(readCard[1], HEX);
+//    myFile.print(readCard[2], HEX);
+//    myFile.print(readCard[3], HEX);
 
-    DataBucket = rfid.uid.uidByte[0] << 24;
-    DataBucket += rfid.uid.uidByte[1] << 16;
-    DataBucket += rfid.uid.uidByte[2] << 8;
-    DataBucket += rfid.uid.uidByte[3];
+    ///////////////////////////////////////////
+    for (int i = 0; i < 4; i++) {
+    
+      byte lowerByte = (readCard[i] & 0x0F);
+      int lowerInt = (int) lowerByte;
+      char lowerChar[1];
+      itoa(lowerInt,lowerChar,16);
+  
+      byte upperByte = (readCard[i] >> 4);
+      int upperInt = (int) upperByte;
+      char upperChar[1];
+      itoa(upperInt,upperChar,16);
+  
+      int j=i*2;
+      int k=j+1;
+      write_buffer[k] = lowerChar[0];
+      write_buffer[j] = upperChar[0];    
+    }
 
-    myFile.print(DataBucket);
+    Serial.println(write_buffer);
+    myFile.print(write_buffer);
+    
+
+    //////////////////////////////////////////
+
     myFile.print(",");
     // close the file:
     myFile.close();
@@ -160,7 +219,6 @@ void WriteMaster() {
     Serial.println("error opening Master.txt");
   }
 }
-
 void WriteOrdinary() {
   pinMode(SD_CS, OUTPUT);
   digitalWrite(SD_CS, LOW);
@@ -186,10 +244,10 @@ void WriteOrdinary() {
     */
 
 
-    myFile.print(readCard[0]);
-    myFile.print(readCard[1]);
-    myFile.print(readCard[2]);
-    myFile.print(readCard[3]);
+    myFile.print(readCard[0], HEX);
+    myFile.print(readCard[1], HEX);
+    myFile.print(readCard[2], HEX);
+    myFile.print(readCard[3], HEX);
 
     myFile.print(",");
     // close the file:
@@ -218,30 +276,89 @@ void LCDInit () {
 void Authorize() {
     //Actuate Relay
   }
-void CheckMatch() { // changed from boolean
-
-  pinMode (7, OUTPUT);
-  digitalWrite(7, LOW);
-  myFile = SD.open("Master.txt");
-
-  if (!myFile) {
-    Serial.print("The text file cannot be opened");
-    while(1);
+void CheckMaster() { // changed from boolean
+    RFIDInit();
+  // Getting ready for Reading Tags/Cards
+  if ( ! rfid.PICC_IsNewCardPresent()) { //If a new Access Card is placed to RFID reader continue
+    return 0;
   }
+  if ( ! rfid.PICC_ReadCardSerial()) {   //Since a Tag is placed, get Serial and continue
+    return 0;
+  }
+  // Assuming Tags have 4 byte UID, others may have 7 (Reminder)
+  Serial.println(F("Checking If Master:"));
+
+  for (int i = 0; i < 4; i++) {
+    readCard[i] = rfid.uid.uidByte[i];
+    Serial.print(readCard[i], HEX);
+  }
+      
+      Serial.println("******************************");
+      rfid.PICC_HaltA(); // Stop reading
+      return 1;
+      
+    pinMode (7, OUTPUT);
+    digitalWrite(7, LOW);
+    myFile = SD.open("Master.txt");
+
+   if (myFile) {
+    Serial.println("Master.txt:");
+
+//    // read from the file until there's nothing else in it:
+//    while (myFile.available()) {
+////     boss = myFile.read();
+////     if (boss){
+//  //    Serial.println(boss);
+//     }
+    }
+//    Serial.println(boss);
+    // close the file:
+    myFile.close();
+  }
+
+//  while (myFile.available()) {
+//    //boss = myFile.readStringUntil(',');
+//    Serial.write(myFile.read());
+//            if (boss == *(unsigned long*)DataBucket, HEX) {
+//            Serial.println("||||||||||||||||||||||||||||||");
+//            Serial.println(boss);
+//            Serial.println("That's a Master Tag");
+//            Serial.println("||||||||||||||||||||||||||||||||");
+//            IsMaster = true;
+//            break;
+//            }
+//            else {
+//            Serial.println("Not Master");
+//            IsMaster = false;
+//            break;
+//            }
+//  myFile.close();
+//  }
  
-  while (myFile.available()) {
-    boss = myFile.readStringUntil(',');
-            if (boss == *(unsigned long*)DataBucket) { //if (buffer == *&readfile) {
-            Serial.println("That's a Master Tag");
-            break;
-            }
-            else {
-            myFile.close();
-            Serial.println("No Master Tag");
-            break;
-            }
-  }
-
-  myFile.close();
+//}}
+void CheckOrdinary(){
+//  pinMode (7, OUTPUT);
+//  digitalWrite(7, LOW);
+//           myFile = SD.open("Ordinary.txt");
+//           while (myFile.available()&&(IsMaster==false)){
+//            boss = myFile.readStringUntil(',');
+//             if (boss == *(unsigned long*)DataBucket,HEX) {
+//              Serial.println("Ordinary Tag");
+//              break;
+//             }
+//             else {
+//              Serial.println("Unknown Tag");
+//              break;
+//             }
+//              myFile.close();
+//      }
+}
+void SD_Disable() {
+  digitalWrite(SD_POWER, HIGH);
+  digitalWrite(SD_CS, HIGH);
+}
+void SD_Enable() {
+  digitalWrite(SD_POWER, LOW);
+  digitalWrite(SD_CS, LOW);
 }
 
